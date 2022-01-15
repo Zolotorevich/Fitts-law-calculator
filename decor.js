efbcedc444c76ca0da0cb435122dd02a315fee05
Roman Zolotorevich <roman@zolotorevich.com>
2022-01-15 19:55:13 +0300

    hide start screen function

--- decor.js
+++ decor.js
@@ -1,4 +1,16 @@
 //transition from start screen to image view
 function hideStartScreen() {
-	
+	//hide start screen container
+	$('#startScreen').hide();
+
+	//display image view bar
+	$('#imageControlsBar').css({'display' : 'flex'});
+
+	//display image view container
+	$('#imageViewContainer').show();
+
+	//scroll image view container to the top
+	$([document.documentElement, document.body]).animate({
+        scrollTop: $("#imageControlsBar").offset().top
+    }, 1000);
 }
